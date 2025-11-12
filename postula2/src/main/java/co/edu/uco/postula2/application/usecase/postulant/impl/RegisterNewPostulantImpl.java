fec3a9a93fa1c272b76d8d33151e129293bd3a29
David-Alzate <alzatedavid0126@gmail.com>
2025-11-11 23:46:53 -0500

    ajistes del notificacionservice

--- postula2/src/main/java/co/edu/uco/postula2/application/usecase/postulant/impl/RegisterNewPostulantImpl.java
+++ postula2/src/main/java/co/edu/uco/postula2/application/usecase/postulant/impl/RegisterNewPostulantImpl.java
@@ -3,6 +3,7 @@ package co.edu.uco.postula2.application.usecase.postulant.impl;
 import co.edu.uco.postula2.application.secondaryports.entity.postulant.PostulantEntity;
 import co.edu.uco.postula2.application.secondaryports.mapper.PostulantEntityMapper;
 import co.edu.uco.postula2.application.secondaryports.repository.PostulantRepository;
+import co.edu.uco.postula2.application.secondaryports.service.NotificationService;
 import co.edu.uco.postula2.application.usecase.postulant.RegisterNewPostulant;
 import co.edu.uco.postula2.domain.postulant.PostulantDomain;
 import org.springframework.stereotype.Service;
@@ -11,14 +12,17 @@ import org.springframework.stereotype.Service;
 public class RegisterNewPostulantImpl implements RegisterNewPostulant {
 
     private final PostulantRepository repository;
+    private final NotificationService notificationService;
 
-    public RegisterNewPostulantImpl(PostulantRepository repository) {
+    public RegisterNewPostulantImpl(PostulantRepository repository, NotificationService notificationService) {
         this.repository = repository;
+        this.notificationService = notificationService;
     }
 
     @Override
     public void execute(PostulantDomain domain) {
         PostulantEntity postulantEntity = PostulantEntityMapper.INSTANCE.toEntity(domain);
         repository.save(postulantEntity);
+        notificationService.notifyNewPostulant(domain);
     }
 }
